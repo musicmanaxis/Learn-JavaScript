f7314a1e0e76825959a09b3254886cebb4e39916
Axis <erlia@naver.com>
2024-10-12 01:43:02 +0900

    1

--- 2_dom-control/1_selector.js
+++ 2_dom-control/1_selector.js
@@ -1,4 +1,4 @@
-console.log("hello java");
-let title=document.getElementById('title');
+// 1] get 메서드
+let title = document.getElementById('title');
 console.log(title);
-title.textContent='헬스 3대운동';
+title.textContent = '헬스 3대 운동';
