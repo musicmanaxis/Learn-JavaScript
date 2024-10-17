fafda6e3503eca6f623acbc386ae78b0c08b6e3d
Axis <erlia@naver.com>
2024-10-18 06:53:24 +0900

    8

--- 2_dom-control/3_javascript/array.js
+++ 2_dom-control/3_javascript/array.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // // 1] 배열 생성
-// const fruits = ['사과', '바나나', '딸기', '파인애플'];
+// const fruits = ['사과', '바나나', '딸기', '파인애플'];   //이방식이 많이 쓰임
 // // const fruits = new Array('사과', '바나나', '딸기');
 // console.log('fruits: ', fruits);
 
@@ -13,40 +13,40 @@
 // // 2] 자주 사용하는 배열 API
 // // length
 // console.log('fruits.length: ', fruits.length);
-// console.log('마지막 값: ', fruits[fruits.length - 1]);
+// console.log('마지막 값: ', fruits[fruits.length - 1]);  배열의 마지막 값을 가져오는 방법
 
-// // push()
+// // push()                                  // 배열의 끝에 값을 추가, 많이 사용함
 // fruits.push('귤');
 // console.log(fruits);
 
-// // forEach()
-// fruits.forEach(function (item, index) {
+// // forEach()    
+// fruits.forEach(function (item, index) {   //forEach()에 인자로 함수를 넘기는 것을 콜백함수라고 한다.
 //   console.log(index, item);
 // });
 
-// // pop()
-// // fruits.pop();
+// // pop()   
+// // fruits.pop();                           //배열의 끝에서부터 값을 제거
 // // let removedItem = fruits.pop();
 // // console.log('removedItem: ', removedItem);
 // // console.log('제거완료: ', fruits);
 
 // // shift()
-// // fruits.shift();
+// // fruits.shift();                          //배열의 앞에서부터 값을 제거
 // // fruits.shift();
 // // console.log(fruits);
 
-// // unshift()
+// // unshift()                              //배열의 앞에서부터 값을 추가
 // fruits.unshift('수박');
 // console.log(fruits);
 
-// // indexOf()
-// const index = fruits.indexOf('토마토');
+// // indexOf()                               //배열요소의 위치검색
+// const index = fruits.indexOf('토마토');     //찾고자 하는게 없다면 -1을 반환
 // console.log('index: ', index);
 
 // // splice()
 // const findIndex = fruits.indexOf('파인애플');
 // console.log('findIndex: ', findIndex);
-// fruits.splice(findIndex, 1);
+// fruits.splice(findIndex, 1);               //제거하고 싶은 인데스 삽입
 // console.log(fruits);
 
 // 3] 구조 분해 할당
