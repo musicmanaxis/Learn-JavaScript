9eb0b73d168087c2726d9058cc1d6f5e7722ceda
Erlia <erlia@naver.com>
2024-12-03 19:12:24 +0900

    15

---
+++ 3_javascript/ArrayTest.js
@@ -0,0 +1,10 @@
+
+const fruits1=['apple', 'banana', 'orange'];
+const result=fruits1.join('-');
+console.log(result);
+
+const fruits2='사과, 애플, 망고, 바나나';
+const arr=fruits2.split(',');
+console.log(typeof(arr))
+console.log(arr)
+
